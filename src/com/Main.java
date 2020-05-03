97b8182501247318c6fe271adfc2cf650fc92dfd
Isseii <sebastian.kujawski.98@poczta.fm>
2020-05-03 15:28:44 +0200

    chuj

--- src/com/Main.java
+++ src/com/Main.java
@@ -4,13 +4,14 @@ import com.HuffmanCoding.*;
 import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
 
 //        String data = new String("no nie wiem jak tak twoja szmaciura zrogowaciala niedzwiedzica");
 //        HuffmanNode root = HuffmanTreeMaker.makeTree(data);
@@ -43,13 +44,20 @@ public class Main {
                     dictionaryOut = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
                     dictionaryOut.writeObject(root);
                     dictionaryOut.close();
+                    socket.close();
                 }catch(IOException ex){
                     System.out.println(ex.getMessage());
                 }
                 DataOutputStream dos = null;
+                try {
+                    socket = new Socket(args[1], Integer.parseInt(args[2]));
+                    System.out.println("Connected");
+                } catch(IOException ex){
+                    System.out.println(ex.getMessage());
+                }
                 try{
                     dos = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
-                    dos.write(encodedString.length());
+                    dos.writeInt(encodedString.length());
                     dos.write(encode);
                     dos.close();
                 }catch (IOException ex){
@@ -58,8 +66,11 @@ public class Main {
                 break;
             case "r":
                 Socket rSocket = null;
+                ServerSocket server = null;
                 try{
-                    rSocket = new Socket(args[1], Integer.parseInt(args[2]));
+                    server = new ServerSocket(Integer.parseInt(args[2]));
+                    System.out.println("Waiting for clinet...");
+                    rSocket = server.accept();
                     System.out.println("Connected");
                 }catch (IOException ex){
                     System.out.println(ex.getMessage());
@@ -70,23 +81,33 @@ public class Main {
                      dictionaryIn = new ObjectInputStream(new BufferedInputStream(rSocket.getInputStream()));
                      rRoot =(HuffmanNode) dictionaryIn.readObject();
                      dictionaryIn.close();
+                     rSocket.close();
                 }catch(IOException | ClassNotFoundException ex){
                     System.out.println(ex.getMessage());
                 }
                 int actualEncodedStringLength = 0;
                 byte[] encoded = null;
                 DataInputStream in = null;
+                
+                try{
+                    rSocket = server.accept();
+                    System.out.println("Connected");
+                }catch (IOException ex){
+                    System.out.println(ex.getMessage());
+                }
                 try{
                     in = new DataInputStream(new BufferedInputStream(rSocket.getInputStream()));
                     actualEncodedStringLength = in.readInt();
                     encoded = new byte[(int) Math.ceil((double) actualEncodedStringLength/8)];
                     for(int i = 0; i < encoded.length; i++){
-                        encoded[i] = in.readByte();
+                       encoded[i] = in.readByte();
                     }
+                    in.close();
                 }catch(IOException ex){
                     System.out.println(ex.getMessage());
                 }
                 String decoded = HuffmanDecode.decode(rRoot, encoded, actualEncodedStringLength);
+                System.out.println(decoded);
         }
     }
 }
